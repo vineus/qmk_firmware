528a2b9a045b3bc9ba5a4028900a2d8d529aa468
Vincent de Lagabbe <vincent.delagabbe@alan.eu>
2021-02-27 19:48:15 +0100

    add keyboard cat

--- keyboards/kyria/keymaps/vineus/config.h
+++ keyboards/kyria/keymaps/vineus/config.h
@@ -18,16 +18,27 @@
 
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
-#endif
-
-#ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 150
+  #define OLED_TIMEOUT 300000
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
+
+#define SPLIT_USB_TIMEOUT 1000
+
+#define MASTER_LEFT
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+#define OLED_FONT_END 127
+
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves.
+    //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression.
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_SLEEP
+#endif
