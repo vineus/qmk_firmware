c756123d39a5efff3cb8324d0dabefc77bd67136
Vincent de Lagabbe <vincent.delagabbe@alan.eu>
2021-03-01 14:57:37 +0100

    improvements

--- keyboards/kyria/keymaps/vineus/config.h
+++ keyboards/kyria/keymaps/vineus/config.h
@@ -22,8 +22,8 @@
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-#define SPLIT_USB_DETECT
-#define NO_USB_STARTUP_CHECK
+//#define SPLIT_USB_DETECT
+//#define NO_USB_STARTUP_CHECK
 
 #define SPLIT_USB_TIMEOUT 1000
 
