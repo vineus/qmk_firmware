fa3c4907b70806a5dab07bc1eea7d02778e2558a
Vincent de Lagabbe <vincent.delagabbe@alan.eu>
2021-03-01 11:37:56 +0100

    Remove enter key I always tapped on

--- keyboards/kyria/keymaps/vineus/keymap.c
+++ keyboards/kyria/keymaps/vineus/keymap.c
@@ -44,15 +44,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |Ctrl/BS |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? | Enter  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |ScrSht| GUI  | Enter| Space| Esc  |  | Enter| Space| Tab  | Bksp | Emoji|
- *                        |      |      | Alt  |      | Lower|  | Raise|      |      |      |      |
+ *                        |ScrSht| GUI  | Enter| Esc  |Space |  | Space| Enter| Tab  | Bksp | Emoji|
+ *                        |      |      | Alt  |Lower |      |  |      | Raise|      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
       LT(_RAISE, KC_ESC), KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       MT(MOD_LSFT, KC_TAB) ,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_LSFT, KC_QUOT),
       MT(MOD_LCTL, KC_BSPC),  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-              KC_CPYP, KC_LGUI, MT(MOD_LALT, KC_ENT), KC_SPC, LT(_LOWER, KC_ESC), LT(_RAISE, KC_ENT), KC_SPC, KC_TAB,  KC_BSPC, KC_EMOJI
+              KC_CPYP, KC_LGUI, MT(MOD_LALT, KC_ENT), LT(_LOWER, KC_SPC), KC_SPC, KC_SPC, LT(_RAISE, KC_SPC), KC_TAB,  KC_BSPC, KC_EMOJI
     ),
 /*
  * Lower Layer: Symbols
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
       _______, _______, _______, _______, _______, _______,                                     _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_RGHT,   KC_UP, KC_DOWN, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                                 KC_CAPP, _______, _______, KC_CLPB, _______, _______, _______, _______, _______, _______
+                                 KC_CAPP, _______, _______, _______, KC_CLPB, _______, _______, _______, _______, _______
     ),
 /*
  * Raise Layer: Number keys, media, navigation
